361ccd5d6c7eee531224c7d2d32757a8be887924
Dannar Mawardi <dmawardi@gmail.com>
2019-07-07 20:21:05 +1000

    Added Gem Reset and reset match functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,93 @@
 // Wait till HTML document to load prior to executing
 $(document).ready(function () {
     // Declare variables
-    var targetCounter = 0;
+    var targetCounter = 200;
     var currentCounter = -1;
     var wins = 0;
     var loss = 0;
 
 
     var gemBar = $('.gemBar');
-    console.log(gemBar);
+
+    
+
+    function startNewMatch() {
+        // Randomize target counter
+        targetCounter = Math.floor(Math.random() * 150) + 1;
+
+        // Reset counter
+        currentCounter = 0;
+
+        // Add code to use randomize gems and images function
+        resetAndRandomGems();
+
+    }
+
+    // Create function to randomize and fill gem
+
+    // Create function to randomize images
+
 
 
     // Generate Gems and socket into the inventory with values
+    function resetAndRandomGems() {
+        // Reset gems in bar
+        gemBar.empty();
 
-    for (var i = 0; i < 5; i++) {
-        // Declar Variables
-        var gemSocket = $('<img>');
-
-
-        console.log('gemsocket:' + gemSocket);
-        console.log('index:' + i);
-
-        // Add class for event handler
-        gemSocket.addClass('gem-image');
-        // Add Bootstrap Class
-        gemSocket.addClass('img-thumbnail');
-
-        gemSocket.attr('data-gemValue', i);
-        gemSocket.attr('height', '200px');
-        gemSocket.attr('width', '200px');
+        // Generate 5 gems in place in gem bar
+        for (var i = 0; i < 5; i++) {
+            // Declare Variables
+            var gemSocket = $('<img>');
+            var gemValue = 0;
 
 
-        console.log(gemSocket.attr('data-gemValue'));
+            console.log('gemsocket:' + gemSocket);
+            console.log('index:' + i);
 
-        gemBar.append(gemSocket);
+            // Add class for event handler
+            gemSocket.addClass('gem-image');
+            // Add Bootstrap Class
+            gemSocket.addClass('img-thumbnail');
+
+            // Get random value for gem
+            gemValue = Math.floor(Math.random() * 50);
+
+            // Apply attributes
+            gemSocket.attr('data-gemValue', gemValue);
+            gemSocket.attr('height', '200px');
+            gemSocket.attr('width', '200px');
+
+
+            console.log(gemSocket.attr('data-gemValue'));
+
+            // Append gemSocket to gemBar
+            gemBar.append(gemSocket);
+        }
     }
+
+
+
 
     $('.gem-image').on('click', function () {
         // Get data-gemValue and add to counter
         var increaseValue = $(this).attr('data-gemValue')
 
-        currentCounter = currentCounter + increaseValue;
+        currentCounter = parseInt(currentCounter) + parseInt(increaseValue);
 
-        console.log('currentCounter:'+currentCounter);
-        console.log('increased by: '+increaseValue);
+        console.log('currentCounter:' + currentCounter);
+        console.log('increased by: ' + increaseValue);
 
         // Check if current counter is over or below
         if (currentCounter > targetCounter) {
-            console.log('Overshot! You lose!');
             loss++;
+            console.log('Overshot! You lose! losses: ' + loss);
+
         }
         // If the counter meets the target
         else if (currentCounter === targetCounter) {
-            console.log('You win!');
             win++;
+            console.log('You win! wins: ' + wins);
+
         }
 
 
